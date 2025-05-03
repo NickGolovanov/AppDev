8e93f99ab45d8b8425ca648ffd75badd8e8d4dd1
NickGolovanov <nikita.golovanov@student.nhlstenden.com>
2025-05-03 15:16:18 +0200

    initialized project structure

---
+++ AppDev/AppDev/AppDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDevApp.swift
+//  AppDev
+//
+//  Created by Nikita Golovanov on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AppDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppDev/AppDevTests/AppDevTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppDevTests.swift
+//  AppDevTests
+//
+//  Created by Nikita Golovanov on 5/3/25.
+//
+
+import Testing
+@testable import AppDev
+
+struct AppDevTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
