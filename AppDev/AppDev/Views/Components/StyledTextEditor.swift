fbdbaafdad6332003da4d023eae52fcbea9c371c
0Empty0 <garg.victor11@gmail.com>
2025-05-09 01:36:21 +0200

    add the create event page

---
+++ AppDev/AppDev/Views/Components/StyledTextEditor.swift
@@ -0,0 +1,36 @@
+//
+//  StyledTextEditor.swift
+//  AppDev
+//
+//  Created by Viktor Harhat on 09/05/2025.
+//
+
+import SwiftUI
+
+struct StyledTextEditor: View {
+    @Binding var text: String
+    var placeholder: String
+    
+    var body: some View {
+        TextEditor(text: $text)
+            .padding(12)
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color(hex: 0xD1D5DB))
+            )
+            .scrollContentBackground(.hidden)
+            .placeholder(when: text.isEmpty, alignment: .topLeading) {
+                Text(placeholder)
+                    .padding(12)
+                    .foregroundColor(Color(hex: 0xADAEBC))
+                    .font(.custom("Poppins-Regular", size: 16))
+            }
+    }
+}
+
+#Preview {
+    @Previewable @State var text: String = ""
+    var placeholder: String = "Placeholder"
+    
+    StyledTextEditor(text: $text, placeholder: placeholder).frame(height: 128)
+}
