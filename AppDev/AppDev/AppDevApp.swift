70960d4c18974a7dd3bcfbe41bba90638b3df86f
Mihail177 <mihail.josan@student.nhlstenden.com>
2025-05-27 12:18:51 +0200

    Implemented that the flow of the app starts with the log in if not authentificated

--- AppDev/AppDev/AppDevApp.swift
+++ AppDev/AppDev/AppDevApp.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAuth
 
 @main
 struct AppDevApp: App {
     // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @StateObject private var authViewModel = AuthViewModel()
     
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            if authViewModel.isAuthenticated {
+                MainTabView()
+            } else {
+                LoginView()
+            }
+        }
+    }
+}
+
+class AuthViewModel: ObservableObject {
+    @Published var isAuthenticated = false
+    
+    init() {
+        // Listen for authentication state changes
+        Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            DispatchQueue.main.async {
+                self?.isAuthenticated = user != nil
+            }
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("Error signing out: \(error.localizedDescription)")
         }
     }
 }
