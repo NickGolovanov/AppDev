759555592eebbdad680be7e75647c5c0d6d5fc8e
Bill <do.do@student.nhlstenden.com>
2025-06-13 12:04:16 +0200

    implemented Google-Sign In

---
+++ AppDev/AppDev/Application.swift
@@ -0,0 +1,26 @@
+//
+//  Application.swift
+//  AppDev
+//
+//  Created by Informatica Emmen on 12/06/2025.
+//
+
+import SwiftUI
+import UIKit
+
+final class Application_utility{
+    static var rootViewController: UIViewController{
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else{
+            return .init()
+            
+        }
+        
+        guard let root = screen.windows.first?.rootViewController else {
+            return .init()
+        }
+        
+        return root
+    }
+    
+    
+}
