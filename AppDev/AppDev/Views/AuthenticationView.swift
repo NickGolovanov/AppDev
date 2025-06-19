39eafbcc3119e7e7cb2e85276882e2c6b79af58e
Bill <do.do@student.nhlstenden.com>
2025-06-19 15:36:56 +0200

    having a little trouble with the Profile Page, right now firebase has shown Google users, also they can see their profile edit changes on Database,but the profile page does not fetch data for Google users, still fixing it

--- AppDev/AppDev/Views/AuthenticationView.swift
+++ AppDev/AppDev/Views/AuthenticationView.swift
@@ -47,6 +47,31 @@ class AuthenticationView: ObservableObject{
                 }
                 guard let user = res?.user else { return }
                 print(user)
+                
+                //----Added code for user document testing----
+                let db = Firestore.firestore()
+                let userRef = db.collection( "users" ).document( user.uid )
+                userRef.getDocument { document, error in
+                    if let document = document, document.exists {
+                        // User document already exists, do nothin
+                    } else {
+                        let userData: [String: Any] = [
+                            "email": user.email ?? "",
+                            "fullname": user.displayName ?? "",
+                            "username": "@" + (user.displayName ?? "").lowercased().replacingOccurrences(of: " ", with: ""),
+                            "description": "",
+                            "profileImageUrl": user.photoURL?.absoluteString ?? ""
+                        ]
+                        userRef.setData(userData) { error in
+                            if let error = error {
+                                print ("Error creating user document: \(error.localizedDescription)")
+                            } else {
+                                print ("User document created for Google user")
+                            }
+                        }
+                    }
+                }
+                //---------------------
             }
         }
         
